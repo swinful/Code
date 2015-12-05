5cd658754db1fda8b7b6dac8d72b6a44b547e909
Samuel A. Winful <samuel@winful.com>
2015-12-05 00:39:21 -0500

    Problem 5 complete.

--- Swift/CSPS01_Console/CSPS01_Console/Lab04.swift
+++ Swift/CSPS01_Console/CSPS01_Console/Lab04.swift
@@ -11,11 +11,27 @@
 // in numbers until you read an even number, in which case you should print out how many 
 // odd numbers you read. (Should you use a while or a for? Do you know how many numbers 
 // the user is going to type ahead of time?)
+// ---
+// Problem 3
+// Write a program that accepts integers from the keyboard, and displays a running total
+// after each integer. The program should stop after the user enters 0.
+// ---
+// Problem 4
+// Write a program that asks the user for a positive integer, and prints that many 1's on 
+// the screen. If the user does not enter a positive number, keep asking them for one 
+// until they do so (what kind of loop do you need for this?) For example, if the user 
+// entered 7, your program would print:
+// 1111111
+
+
 
 
 import Foundation
 
 class Lab04 {
+  
+  // MARK: Common Variables
+  var positiveInteger = 0
   
   // MARK: Problem One
   
@@ -43,9 +59,54 @@ class Lab04 {
     print("We read \(count) odd numbers.")
   }
   
+  // MARK: Problem Three
+  func problemThree() {
+    IO.promptInput(with: "\n\nLab 4, Assignment #3\n")
+    IO.promptInput(with: "Please enter integers to total, ending with 0: ")
+    var enteredInt = IO.getInput().integerValue
+    var sum = 0
+    while (enteredInt != 0) {
+      sum += enteredInt
+      print("Added: \(enteredInt); total = \(sum)")
+      IO.promptInput(with: "Enter an integer: ")
+      enteredInt = IO.getInput().integerValue
+    }
+  }
+  
+  // MARK: Problem Four
+  func problemFour() {
+    IO.promptInput(with: "Enter positive integer: ")
+    let posInt = IO.getInput().integerValue
+    for _ in 1...posInt {
+      IO.promptInput(with: "1")
+    }
+    print("")
+    
+  }
+  
+  // MARK: Problem Five
+  func problemFive() {
+    IO.promptInput(with: "Enter Positive Integer: ")
+    positiveInteger = IO.getInput().integerValue
+    while positiveInteger < 1 {
+      IO.promptInput(with: "Enter Positive Integer: ")
+      positiveInteger = IO.getInput().integerValue
+    }
+    
+    if positiveInteger > 0 {
+      for i in 1...positiveInteger {
+        IO.promptInput(with: "\(i)")
+      }
+    }
+    
+    
+  }
   
   // MARK: Main run
   func run() {
-    problemTwo()
+    // problemTwo()
+    // problemThree()
+    // problemFour()
+    problemFive()
   }
 }
